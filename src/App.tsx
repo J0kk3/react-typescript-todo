352ac46eaf743d564b68d1fbb77dbb196ddc9a70
J0kk3 <joakim_88@msn.com>
2022-10-01 11:40:42 +0200

    [INIT] project start +added Todo.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//components
+import Todos from './components/Todos';
+//styles
+import './App.css';
+
+function App ()
+{
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+//app
+import App from './App';
+//styles
+import './index.css';
+
+const root = ReactDOM.createRoot( document.getElementById( 'root' ) as HTMLElement );
+root.render(
+  <App />
+);
