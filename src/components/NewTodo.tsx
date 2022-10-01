49e45bba92c9178db9a8756735dbc1830acf85db
J0kk3 <joakim_88@msn.com>
2022-10-01 15:14:01 +0200

    [ADD] remove todo with onClick & css

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,34 @@
+//hooks
+import { useRef } from 'react';
+//styles
+import classes from './NewTodo.module.css';
+
+const NewTodo: React.FC<{ onAddTodo: ( text: string ) => void; }> = ( props ) =>
+{
+    const todoTextInputRef = useRef<HTMLInputElement>( null );
+
+    const submitHandler = ( event: React.FormEvent ) =>
+    {
+        event.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value;
+
+        if ( enteredText.trim().length === 0 )
+        {
+            //throw an error
+            return;
+        }
+
+        props.onAddTodo( enteredText );
+    };
+
+    return (
+        <form onSubmit={ submitHandler } className={ classes.form }>
+            <label htmlFor="text">Todo text</label>
+            <input id="text" type="text" ref={ todoTextInputRef } />
+            <button>Add Todo</button>
+        </form>
+    );
+};
+
+export default NewTodo;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,10 @@
-const TodoItem: React.FC<{ text: string; }> = ( props ) =>
+//styles
+import classes from "./TodoItem.module.css";
+
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = ( props ) =>
 {
     return (
-        <li>{ props.text }</li>
+        <li className={ classes.item } onClick={ props.onRemoveTodo }>{ props.text }</li>
     );
 
 };
